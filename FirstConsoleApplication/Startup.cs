0091a9f15a6cd352710b6a2be216e0cac547eed8
smoumita19 <smoumita19@yahoo.com>
2022-02-05 16:25:00 +0530

    Adding the project folder

---
+++ FirstConsoleApplication/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FirstConsoleApplication
+{
+    public class Startup
+    {
+        public void ConfigurationServices()
+        {
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
