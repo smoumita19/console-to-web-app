bec42bb6f048de34f57d33b548b1392b3202efbd
smoumita19 <smoumita19@yahoo.com>
2022-02-06 22:18:05 +0530

    Adding routing

--- FirstConsoleApplication/Startup.cs
+++ FirstConsoleApplication/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace FirstConsoleApplication
 {
@@ -8,11 +9,20 @@ namespace FirstConsoleApplication
     {
         public void ConfigurationServices()
         {
-
+            
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseRouting();
 
+            app.UseEndpoints(endpoints =>
+           {
+               endpoints.MapGet("/", async context =>
+               {
+                   await context.Response.WriteAsync("Hello from endpoints");
+               });
+
+           });
         }
     }
 }
