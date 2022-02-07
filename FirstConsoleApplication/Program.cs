b941353ebe21cd27cb6c08a5caf7f12768c33bd6
Moumita Sen <52632685+smoumita19@users.noreply.github.com>
2022-02-07 09:22:03 +0530

    Update Program.cs

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -7,7 +7,15 @@ namespace FirstConsoleApplication
         static void Main(string[] args)
         {
             //Will print in the console
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(agrs).Build().Run();
+        }
+        
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults( webBuilder =>  
+            {
+                webBuilder.useStartup<Startup>();
+            });
         }
     }
 }
